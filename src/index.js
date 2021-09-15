ecdc63cbcb4c451a210b267ec37afa2d9c2095f9
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-09-15 12:55:56 +0200

    initial redux setup to manage app state better

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
