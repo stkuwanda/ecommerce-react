7805e6b2702ada32156425aef39d84daaa00e869
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-09-15 16:07:04 +0200

    updated the Header component to use state pulled from the redux store using the connect HOC, updated App componet to stop the Header component from get its state set by the App component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
@@ -30,4 +31,6 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({currentUser: state.user.currentUser});
+
+export default connect(mapStateToProps)(Header);
