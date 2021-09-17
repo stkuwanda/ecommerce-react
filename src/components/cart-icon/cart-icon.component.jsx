c01a9cc8a01ef97ec4a7963b6ceb924cd9ff8f12
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-09-17 07:57:03 +0200

    added a reducing selector along with redux state to calculate the number of cart items in the cart-icon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,15 +4,19 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleHideCartDropdown } from "../../redux/cart/cart.actions";
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({toggleHideCartDropdown}) => (
+const CartIcon = ({ toggleHideCartDropdown, cartItemCount }) => (
   <div className='cart-icon' onClick={toggleHideCartDropdown}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{cartItemCount}</span>
   </div>
 );
 
-const mapDispatchToProps = dispatch => {
-  return {toggleHideCartDropdown: () => dispatch(toggleHideCartDropdown())}
-}
+const mapDispatchToProps = (dispatch) => {
+  return { toggleHideCartDropdown: () => dispatch(toggleHideCartDropdown()) };
+};
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItemCount: cartItems.reduce((acc, item) => item.quantity + acc, 0),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
