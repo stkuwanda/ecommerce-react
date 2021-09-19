e4f51415822d6902a2e3a2aa2fd76b002d9d2fc1
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-09-19 06:41:14 +0200

    updated directory component to be a functional component

--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -3,23 +3,15 @@ import { connect } from "react-redux";
 import "./directory.styles.scss";
 import MenuItem from "../menu-item/menu-item.component";
 
-class Directory extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    const { sections } = this.props;
-
-    return (
-      <div className='directory-menu'>
-        {sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const Directory = ({ sections }) => {
+  return (
+    <div className='directory-menu'>
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
 
 const mapStateToProps = ({ directory: { sections } }) => ({ sections });
 
