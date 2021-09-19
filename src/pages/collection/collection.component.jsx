c245043d766fd857cf0fda5c8bd13aecc4cbbc8b
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-09-19 20:00:34 +0200

    created the selectCollection selector and used it to render items from the collection state based on a url param, updated the collection page to render the appropriate items based on the match.params.collectionId

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+import { selectCollection } from "../../redux/shop/shop.selectors";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 import "./collection.styles.scss";
 
-const CollectionPage = ({ match }) => {
-  const { collectionId } = match.params;
+const CollectionPage = ({ collection }) => {
+  console.log(
+    "line 7, collection.component.jsx, CollectionPage, collection prop:",
+    collection
+  );
 
-  return <div className='collection'>COLLECTION PAGE</div>;
+  return (
+    <div className='collection-page'>
+      <h1 className='title'>{collection.title}</h1>
+      <div className='items'>
+        {collection.items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, { match: { params } }) => ({
+  collection: selectCollection(params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,23 @@
 import { createSelector } from "reselect";
 
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 4,
+  womens: 4,
+  mens: 5,
+};
+
 const selectShop = (state) => state.shop;
 
 export const selectShopData = createSelector(
   [selectShop],
   (shop) => shop.SHOP_DATA
 );
+
+export const selectCollection = (collectionUrlParam) =>
+  createSelector([selectShopData], (shopData) =>
+    shopData.find(
+      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  );
