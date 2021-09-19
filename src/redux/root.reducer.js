41d2ed139ceebbdfa107b353b409cdf65353582f
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-09-19 06:37:22 +0200

    created a directory reducer to add directory state information to redux, updated the directory class component to use redux patterns and connector to get the directory state

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -3,12 +3,14 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import cartReducer from "./cart/cart.reducer";
 import userReducer from "./user/user.reducer";
+import directoryReducer from "./directory/directory.reducer";
 
 const persistConfig = { key: "root", storage, whitelist: ["cart"] };
 
 const unpersistedRootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer
 });
 
 const persistedRootReducer = persistReducer(
