1cb703c0ae947251da9bf51d8393933b09b6385b
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-09-19 08:25:11 +0200

    created new collections overview component and wrapped collection previews inside it, used it inside the shop page, renamed corrected typo on file holding the directory component

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import CollectionPreview from "../collection-preview/collection-preview.component";
+import { selectShopData } from "../../redux/shop/shop.selectors";
+import "./collections-overview.styles.scss";
+
+const CollectionsOverview = ({ collections }) => {
+  return (
+    <div className='collection-overview'>
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectShopData,
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,8 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-import { selectShopData } from "../../redux/shop/shop.selectors";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 
-const ShopPage = ({ collections }) => {
-  return (
-    <div className='shop-page'>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
-    </div>
-  );
+const ShopPage = () => {
+  return <div className='shop-page'><CollectionsOverview /></div>;
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectShopData,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
