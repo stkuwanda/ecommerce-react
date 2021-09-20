2569f78312342d18d7c7c19d7a31f27afeb7861c
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-09-20 12:01:09 +0200

    updated collections overview component to remove unnecessary import, updated collection preview component title to navigate on clicking to collection page, updated it's styles to reflect a pointer cursor for the clickable title

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { withRouter } from "react-router";
 import CollectionItem from "../collection-item/collection-item.component";
 import "./collection-preview.styles.scss";
 
-const CollectionPreview = ({ title, items }) => (
-  <div className='collection-preview'>
-    <h1 className='title'>{title.toUpperCase()}</h1>
-    <div className='preview'>
-      {items
-        .filter((item, idx) => idx < 4)
-        .map((item) => (
-          <CollectionItem key={item.id} item={item} />
-        ))}
-    </div>
-  </div>
-);
+const CollectionPreview = ({ title, routeName, items, history, match }) => {
+  //console.log('line 7, collection-preview.component.jsx, CollectionPreview, otherProps:', otherProps);
 
-export default CollectionPreview;
+  return (
+    <div className='collection-preview'>
+      <h1
+        className='title'
+        onClick={() => history.push(`${match.url}/${routeName}`)}
+      >
+        {title.toUpperCase()}
+      </h1>
+      <div className='preview'>
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default withRouter(CollectionPreview);
--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../collection-preview/collection-preview.component";
-import { selectCollections, selectShopData } from "../../redux/shop/shop.selectors";
+import { selectCollections } from "../../redux/shop/shop.selectors";
 import "./collections-overview.styles.scss";
 
 const CollectionsOverview = ({ collections }) => {
