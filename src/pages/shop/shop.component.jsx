9098dd0ceeedb045fa866d0713e2f661114e0fb7
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-09-28 14:08:06 +0200

    reconverted ShopPage into a class based component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,17 +3,23 @@ import { Route } from "react-router-dom";
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
 import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({ match }) => {
-  if (process.env.NODE_ENV === "development") {
-    console.log("line 6, shop.component.jsx, ShopPage, Match object:", match);
-  }
+class ShopPage extends React.Component {
+  render() {
+    const { match } = this.props;
 
-  return (
-    <div className='shop-page'>
-      <Route exact path={match.path} component={CollectionsOverview} />
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-    </div>
-  );
-};
+    if (process.env.NODE_ENV === "development") {
+      console.log("line 6, shop.component.jsx, ShopPage, Match object:", match);
+    }
+    return (
+      <div className='shop-page'>
+        <Route exact path={match.path} component={CollectionsOverview} />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPage}
+        />
+      </div>
+    );
+  }
+}
 
 export default ShopPage;
