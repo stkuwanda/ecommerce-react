ee7c673d0825c335b11d546c15fd0525b5e94fd7
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-10-07 07:40:48 +0200

    refactored and updated shop sagas to use a uniform export and import pattern with the root saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, put, call } from "redux-saga/effects";
+import { takeLatest, put, call, all } from "redux-saga/effects";
 import { ShopActionTypes } from "./shop.types";
 import {
   collection,
@@ -6,39 +6,47 @@ import {
   getDocs,
   convertCollectionsSnapshotToMap,
 } from "../../firebase/firebase.utils";
-import { fetchCollectionsFailure, fetchCollectionsSuccess } from "./shop.actions";
+import {
+  fetchCollectionsFailure,
+  fetchCollectionsSuccess,
+} from "./shop.actions";
 
-export function* fetchCollectionsAsync() {
+function* fetchCollectionsSaga() {
   if (process.env.NODE_ENV === "development") {
     yield console.log(
-      "line 6, shop.sagas.js, fetchCollectionsAsyncAsync, I've been triggered!"
+      "shop.sagas.js, fetchCollectionsSaga, I've been triggered!"
     );
   }
 
   try {
     const collectionRef = collection(firestore, "collections");
-    const snapShot = yield getDocs(collectionRef);
+    const snapShot = yield call(getDocs, collectionRef);
     const collectionsMap = yield call(
       convertCollectionsSnapshotToMap,
       snapShot
     );
     yield put(fetchCollectionsSuccess(collectionsMap));
   } catch (err) {
+    yield put(fetchCollectionsFailure(err.message));
+
     if (process.env.NODE_ENV === "development") {
       console.log(
-        "line 29, shop.sagas.js, fetchCollectionsAsync, Error Retrieving QuerySnapshot:",
+        "shop.sagas.js, fetchCollectionsSaga, Error Retrieving QuerySnapshot:",
         err.message
       );
     }
-    yield put(fetchCollectionsFailure(err.message));
     alert("An unexpected error occurred!");
   }
 }
 
-export function* fetchCollectionsStart() {
+function* fetchCollectionsStartSaga() {
   // takeLatest starts a new fetchCollectionsAsync task whenever it picks up a FETCH_COLLECTIONS_START action
   yield takeLatest(
     ShopActionTypes.FETCH_COLLECTIONS_START,
-    fetchCollectionsAsync
+    fetchCollectionsSaga
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStartSaga),]);
 }
