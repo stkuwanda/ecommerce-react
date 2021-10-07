a524a6df037a43cb19ade19551db542f7e15f834
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-10-07 07:48:58 +0200

    refactored cart.utils.js to improve code readability

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,10 +1,12 @@
 export const addCartItem = (cartItems, cartItem) => {
   const existingItem = cartItems.find((item) => item.id === cartItem.id);
+
   if (existingItem) {
     return cartItems.map((item) =>
       item.id === cartItem.id ? { ...item, quantity: item.quantity + 1 } : item
     );
   }
+
   return [...cartItems, { ...cartItem, quantity: 1 }];
 };
 
@@ -15,8 +17,10 @@ export const removeCartItem = (cartItems, cartItem) => {
         item.quantity -= 1;
         return {...item};
       }
+      
       return item;
     });
   }
+
   return cartItems.filter((item) => item.id !== cartItem.id);
 };
