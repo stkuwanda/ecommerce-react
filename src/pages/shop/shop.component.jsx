10566f1f6ecffb13871de984d8c6aec5d6bc9316
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-10-13 16:34:33 +0200

    updated shop page component to use useEffect

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,40 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route } from "react-router-dom";
 import { fetchCollectionsStart } from "../../redux/shop/shop.actions";
 import { connect } from "react-redux";
 import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
 import CollectionContainer from "../collection/collection.container";
 
-class ShopPage extends React.Component {
-  componentDidMount() {
-    this.props.fetchCollections();
+const ShopPage = ({ fetchCollections, match }) => {
+  useEffect(() => {
+    fetchCollections();
+  }, [fetchCollections]);
+
+  if (process.env.NODE_ENV === "development") {
+    console.log("line 23, shop.component.jsx, ShopPage, Match object:", match);
   }
 
-  render() {
-    const { match } = this.props;
-
-    if (process.env.NODE_ENV === "development") {
-      console.log(
-        "line 23, shop.component.jsx, ShopPage, Match object:",
-        match
-      );
-    }
-
-    return (
-      <div className='shop-page'>
-        <Route
-          exact
-          path={match.path}
-          component={CollectionsOverviewContainer}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionContainer}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className='shop-page'>
+      <Route exact path={match.path} component={CollectionsOverviewContainer} />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionContainer}
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollections: () => dispatch(fetchCollectionsStart()),
