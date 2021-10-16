8f275774c7d0336b311929fb0fb3bd6f9abb7b57
Simbarashe Kuwanda <stkuwanda@gmail.com>
2021-10-16 14:09:47 +0200

    refactored the CheckoutPage component to use the useSelector react-redux hook

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStructuredSelector } from "reselect";
-import { connect } from "react-redux";
+import { connect, useSelector, useDispatch } from "react-redux";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import {
@@ -9,41 +9,41 @@ import {
 } from "../../redux/cart/cart.selectors";
 import "./checkout.styles.scss";
 
-const CheckoutPage = ({ cartItems, totalPrice }) => (
-  <div className='checkout-page'>
-    <div className='checkout-header'>
-      <div className='header-block'>
-        <span>Product</span>
-      </div>
-      <div className='header-block'>
-        <span>Description</span>
-      </div>
-      <div className='header-block'>
-        <span>Quantity</span>
-      </div>
-      <div className='header-block'>
-        <span>Price</span>
-      </div>
-      <div className='header-block'>
-        <span>Remove</span>
-      </div>
-    </div>
-    {cartItems.map((item) => (
-      <CheckoutItem key={item.id} cartItem={item} />
-    ))}
-    <div className='total'>TOTAL: ${totalPrice}</div>
-    <div className='test-warning'>
-      *Please use the following test credit card for payments*
-      <br />
-      4242 4242 4242 4242 - Exp: 01/24 - CVV: 123
-    </div>
-    <StripeCheckoutButton price={totalPrice} />
-  </div>
-);
+const CheckoutPage = () => {
+  const cartItems = useSelector(selectCartItems);
+  const totalPrice = useSelector(selectCartTotalPrice);
 
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  totalPrice: selectCartTotalPrice,
-});
+  return (
+    <div className='checkout-page'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
+      </div>
+      {cartItems.map((item) => (
+        <CheckoutItem key={item.id} cartItem={item} />
+      ))}
+      <div className='total'>TOTAL: ${totalPrice}</div>
+      <div className='test-warning'>
+        *Please use the following test credit card for payments*
+        <br />
+        4242 4242 4242 4242 - Exp: 01/24 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={totalPrice} />
+    </div>
+  );
+};
 
-export default connect(mapStateToProps)(CheckoutPage);
+export default CheckoutPage;
